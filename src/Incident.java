15031adff00f0cc6d9355f13f6c82635bf5cba25
William Moses <w.moses1@newcastle.ac.uk>
2021-02-09 23:33:46 +0700

    Updated Incident.java and added main menu on ReportingIO.java.

--- src/Incident.java
+++ src/Incident.java
@@ -1,2 +1,45 @@
+import java.util.Scanner;
+
 public class Incident {
+
+    double value;
+    String postcode;
+    String month;
+    String year;
+
+    public Incident(double value, String postcode, String month, String year) {
+        this.value = value;
+        this.postcode = postcode;
+        this.month = month;
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Incident{" +
+                "value=" + value +
+                ", postcode='" + postcode + '\'' +
+                ", month='" + month + '\'' +
+                ", year='" + year + '\'' +
+                '}';
+    }
+
+    public static void assign() {
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Enter total value stolen: ");
+        double value = Double.parseDouble(s.nextLine());
+
+        System.out.println("Enter postcode: ");
+        String postcode = s.nextLine();
+
+        System.out.println("Enter month: ");
+        String month = s.nextLine();
+
+        System.out.println("Enter year: ");
+        String year = s.nextLine();
+
+        Incident i = new Incident(value, postcode, month, year);
+        System.out.println(i);
+    }
 }
--- src/ReportingIO.java
+++ src/ReportingIO.java
@@ -1,7 +1,30 @@
+import java.util.Scanner;
+
 public class ReportingIO {
 
     public static void main(String[] args) {
+        System.out.println("""
+                Do you want to report a crime?
+                1 - Report a crime
+                2 - Exit
+                """);
 
+        int choice;
+
+        Scanner s = new Scanner(System.in);
+        choice = s.nextInt();
+
+        while (true) {
+            if (choice == 1) {
+                Incident.assign();
+            }
+            else if (choice == 2){
+                System.out.println("Terminating program.");
+                System.exit(1);
+            }
+            else {
+                System.out.println("Please select 1 or 2.");
+            }
+        }
     }
-
 }
