cef4dd34b99c4a2fc8cfc8075f3185aa327dde23
William Moses <w.moses1@newcastle.ac.uk>
2021-02-10 23:00:28 +0700

    Updated ReportingIO.java, wrong inputs won't be accepted.

--- src/ReportingIO.java
+++ src/ReportingIO.java
@@ -3,28 +3,40 @@ import java.util.Scanner;
 public class ReportingIO {
 
     public static void main(String[] args) {
+
+        menu();
+
+    }
+
+    public static void menu() {
         System.out.println("""
                 Do you want to report a crime?
                 1 - Report a crime
                 2 - Exit
                 """);
+        getInput();
+    }
 
-        int choice;
+    public static void getInput() {
 
-        Scanner s = new Scanner(System.in);
-        choice = s.nextInt();
+        int choice = -1;
+        int min = 1;
+        int max = 2;
 
-        while (true) {
+        while (choice < min || choice > max) {
+            System.out.println("Enter 1 or 2: ");
+            Scanner s = new Scanner(System.in);
+            choice = s.nextInt();
             if (choice == 1) {
                 Incident.assign();
-            }
-            else if (choice == 2){
+            } else if (choice == 2) {
                 System.out.println("Terminating program.");
                 System.exit(1);
-            }
-            else {
-                System.out.println("Please select 1 or 2.");
+            } else {
+                System.out.println("Input " + choice + " is invalid. Please enter 1 or 2.");
             }
         }
     }
 }
+
+
