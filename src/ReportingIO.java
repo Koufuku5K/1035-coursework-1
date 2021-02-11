58963dd0744de0dc9fb31d8dc34925458702da51
William Moses <w.moses1@newcastle.ac.uk>
2021-02-11 23:21:07 +0700

    Updated main menu on ReportingIO.java

--- src/ReportingIO.java
+++ src/ReportingIO.java
@@ -3,40 +3,46 @@ import java.util.Scanner;
 public class ReportingIO {
 
     public static void main(String[] args) {
-
-        menu();
-
+        new ReportingIO().menu();
     }
 
-    public static void menu() {
-        System.out.println("""
+    private void menu() {
+
+        int choice = 0;
+        boolean flag = true;
+
+        Scanner s = new Scanner(System.in);
+
+        while (flag) {
+
+            System.out.println("""
                 Do you want to report a crime?
                 1 - Report a crime
                 2 - Exit
                 """);
-        getInput();
-    }
 
-    public static void getInput() {
+            do {
+                while (!s.hasNextInt()) {
+                    System.out.println("Please input a number.");
+                    s.next();
+                }
+                choice = s.nextInt();
+            }
+            while (choice < 0);
 
-        int choice = -1;
-        int min = 1;
-        int max = 2;
+            switch (choice) {
+                case 1:
+                    District.assignDistrict();
+                    break;
+                case 2:
+                    System.out.println("Quitting program.");
+                    flag = false;
+                    break;
+                default:
+                    System.out.println("Input " + choice + " is invalid. Please enter 1 or 2.");
 
-        while (choice < min || choice > max) {
-            System.out.println("Enter 1 or 2: ");
-            Scanner s = new Scanner(System.in);
-            choice = s.nextInt();
-            if (choice == 1) {
-                Incident.assign();
-            } else if (choice == 2) {
-                System.out.println("Terminating program.");
-                System.exit(1);
-            } else {
-                System.out.println("Input " + choice + " is invalid. Please enter 1 or 2.");
+
             }
         }
     }
 }
-
-
