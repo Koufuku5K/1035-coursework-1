d52c19caada2f3080f160165bb6be07baf03d144
William Moses <w.moses1@newcastle.ac.uk>
2021-02-14 15:22:29 +0700

    Updated Reporting.java

--- src/Reporting.java
+++ src/Reporting.java
@@ -1,2 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Reporting {
+    private List<District> districts;
+    private List<Incident> incidents;
+
+    public Reporting() {
+        districts = new ArrayList<>();
+    }
+
+    public List<District> getDistricts() {
+        return districts;
+    }
+
+    public void setDistricts(List<District> districts) {
+        this.districts = districts;
+    }
+
+    @Override
+    public String toString() {
+        return "Reporting{" +
+                "districts=" + districts +
+                '}';
+    }
+
 }
