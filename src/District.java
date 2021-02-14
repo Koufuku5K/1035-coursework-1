f5853e8645db0b141e8c48e9ce643adc4528e488
William Moses <w.moses1@newcastle.ac.uk>
2021-02-14 15:15:41 +0700

    Updated District.java, added averageValue method.

--- src/District.java
+++ src/District.java
@@ -12,6 +12,11 @@ public class District {
         incidents = new ArrayList<>();
     }
 
+    public District(String name, List<Incident> incidents) {
+        this.name = name;
+        this.incidents = incidents;
+    }
+
     public String getName() {
         return name;
     }
@@ -46,37 +51,43 @@ public class District {
         System.out.println("Enter District Name: ");
         String name = s.nextLine();
 
-        District d = new District(name);
+        List<Incident> incidents = new ArrayList<>();
 
-        Incident i = new Incident(4200, "414152", "January", "2021");
-        Incident i1 = new Incident(4521, "687633", "February", "2021");
-        Incident i2 = new Incident(2200, "876541", "January", "2021");
-        Incident i3 = new Incident(3200, "675193", "February", "2021");
+        District d = new District(name, incidents);
 
-        d.addIncident(i);
-        d.addIncident(i1);
-        d.addIncident(i2);
-        d.addIncident(i3);
+        for (int j = 0; j < 2; j++) {
 
-        System.out.println("""
-                -------------------
-                Current crime list:
-                -------------------""");
-        System.out.println(d);
-        System.out.println(d.getIncident());
+            d.addIncident(Incident.inputIncident());
 
-        d.addIncident(Incident.inputIncident());
+        }
+
+        System.out.println("Enter District Name: ");
+        String name1 = s.nextLine();
+
+        District d1 = new District(name1);
+
+        for (int j = 0; j < 2; j++) {
+
+            d1.addIncident(Incident.inputIncident());
+
+        }
 
         System.out.println(d.highestValue());
 
-        }
+        System.out.println(d.averageValue());
+
+        System.out.println(d1.highestValue());
+
+        System.out.println(d1.averageValue());
+
+    }
 
     public Incident highestValue() {
         double lastValue = 0.0;
         Incident temp = null;
         District d = new District(name);
-        System.out.println("\nThe incident with the highest value stolen is:");
-        System.out.println(d);
+        System.out.println("\nThe incident with the highest value stolen at " + d.getName() +
+                " district is:");
         for (Incident i : incidents) {
             if (i.getValue() > lastValue) {
                 lastValue = i.getValue();
@@ -85,5 +96,18 @@ public class District {
         }
         return temp;
     }
+
+    public int averageValue() {
+        double sum = 0.0;
+        double average = 0.0;
+        District d = new District(name);
+        System.out.println("\nThe average stolen value at " + d.getName() + " district is:");
+        for (Incident i : incidents) {
+            sum = i.getValue() + i.getValue();
+            average = sum / 2;
+            }
+        return (int) average;
+    }
+
 }
 
